96b019f44701ec46577510590fdc98f84eb10e70
titte <titte@telia.com>
2016-02-11 16:11:21 +0100

    Added filter for performance

--- SilverlightApplication1/Controls/SelectableAutoCompleteBox.cs
+++ SilverlightApplication1/Controls/SelectableAutoCompleteBox.cs
@@ -4,6 +4,45 @@ namespace SilverlightApplication1.Controls
 {
     public class SelectableAutoCompleteBox : AutoCompleteBox
     {
+        public SelectableAutoCompleteBox()
+        {
+            FilterMode = AutoCompleteFilterMode.None;
+            MaxDropDownHeight = 350;
+            MinimumPrefixLength = 2;
+            Loaded += SelectableAutoCompleteBox_Loaded;
+            TextChanged += SelectableAutoCompleteBox_TextChanged;      
+        }
+
+        private void SelectableAutoCompleteBox_TextChanged(object sender, System.Windows.RoutedEventArgs e)
+        {
+            counter = 0;
+        }
+
+        int counter;
+        int MaxItemsInDropDown = 50;
+        private void SelectableAutoCompleteBox_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            this.TextFilter = (search, value) =>
+            {
+                System.Diagnostics.Debug.WriteLine(counter);
+                if (value.Length > 0)
+                {
+                    if ((this.counter < MaxItemsInDropDown) &&
+                        (value.ToUpper().StartsWith(search.ToUpper()) ||
+                         value.ToUpper().Contains(search.ToUpper())))
+                    {
+                        this.counter++;
+                        return true;
+                    }
+                    else
+                        return false;
+                }
+                else
+                    return false;
+            };
+            
+        }
+
         public TextBox InnerTextBox
         {
             get
